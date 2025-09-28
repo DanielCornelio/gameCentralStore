07467edb41229da8cb78cfcca9ce83dc34880e14
Paula Cortes-Monroy <ps.paulacortesmonroy@gmail.com>
2025-09-28 17:46:41 -0300

    se agrego busqueda y filtros en pagina de juegos se agrego componente SearchAndFilters con filtros de genero,plataforma y ordenamiento se agrego hook usegameFilters para logica del filtrado se agregp pagina Games con funcionalidad completa del filtrado se agrego datos de juego con ejemplos se agrego ruta para games en configuracion de rutas se agregaron estilos a los componentes

---
+++ client/src/data/games.js
@@ -0,0 +1,37 @@
+export const gamesData = [
+  {
+    id: 1,
+    title: "Resident Evil 0",
+    genre: "Terror",
+    platform: "PC",
+    price: 29.99,
+    image: "/images/resident-evil-0.jpg"
+  },
+  {
+    id: 2,
+    title: "Call of Duty",
+    genre: "Acción",
+    platform: "PC",
+    price: 59.99,
+    image: "/images/cod.jpg"
+  },
+  {
+    id: 3,
+    title: "FIFA 24",
+    genre: "Deportes",
+    platform: "PS5",
+    price: 69.99,
+    image: "/images/fifa24.jpg"
+  },
+  {
+    id: 4,
+    title: "Minecraft",
+    genre: "Aventura",
+    platform: "PC",
+    price: 26.95,
+    image: "/images/minecraft.jpg"
+  }
+];
+
+export const genres = ["Terror", "Acción", "Deportes", "Aventura", "RPG", "Estrategia"];
+export const platforms = ["PC", "PS5", "Xbox", "Nintendo Switch"];
